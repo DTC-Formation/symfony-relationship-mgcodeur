6234ac4e8fa90182f8e25ed9c1647bc1ad13571f
mgcodeur <mgcodeur@gmail.com>
2023-07-25 23:41:55 +0300

    initialisation du projet, mise en place de l'ui (en utilisant symfony Ux)

---
+++ assets/app.js
@@ -0,0 +1,9 @@
+window.$ = window.jQuery = require('jquery');
+
+window.PerfectScrollbar = require('perfect-scrollbar').default;
+require('perfect-scrollbar/css/perfect-scrollbar.css')
+
+require('bootstrap');
+require('./js/scripts/script.js');
+require('./js/scripts/sidebar.large.script.js');
+require('./js/scripts/dashboard.v1.script.js');
